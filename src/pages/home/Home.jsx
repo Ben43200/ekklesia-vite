5287932f16b1b63f07b53f81e1741e6973c75f77
DOLLFUS benjamin <meteoben@gmail.com>
2024-03-12 11:03:13 +0100

    font-size paragraphe intro

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -7,7 +7,7 @@ function Home() {
     "Bienvenue sur le site Ekklêsia Web. Nous accompagnons les Eglises locales dans leur croissance en permettant une plus grande visibilité sur Internet. A l’écoute des besoins spécifiques de chaque paroisse, nous proposons des sites Web clés en main, à prix serrés. Nous concevons également des solutions sur mesure et professionnelles, sécurisées et évolutives.";
   return (
     <main>
-      <p>{homeIntro}</p>
+      <p className="intro">{homeIntro}</p>
       <Card />
     </main>
   );
